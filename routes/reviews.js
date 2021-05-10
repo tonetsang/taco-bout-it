a1d9fa0358271d7bcb6829a6bfc69586b1168b5a
Tony Tsang <offcell@gmail.com>
2021-05-10 16:15:23 -0400

    hmm

--- routes/reviews.js
+++ routes/reviews.js
@@ -17,8 +17,6 @@ router.delete('/deleteReview', ensureAuth, reviewsController.deleteReview)
 
 router.get('/resume/:id', ensureAuth, reviewsController.renderResume)
 
-router.get('/resume/:id', ensureGuest, reviewsController.renderResume)
-
 router.put('/addLike', ensureAuth, reviewsController.addLike)
 
 
